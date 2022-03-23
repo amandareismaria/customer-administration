fb9acdbfb442d14e46fec3aa8b854d81300a13f2
Amanda Reis <amandareismaria@gmail.com>
2022-03-23 17:18:49 -0300

    Tratamento de excecao

---
+++ src/main/java/com/amanda/soap/webservices/customeradministration/soap/exception/CustomerNotFoundException.java
@@ -0,0 +1,16 @@
+package com.amanda.soap.webservices.customeradministration.soap.exception;
+
+import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
+import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
+
+@SoapFault(faultCode = FaultCode.CLIENT)
+public class CustomerNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+
+	}
+
+}
